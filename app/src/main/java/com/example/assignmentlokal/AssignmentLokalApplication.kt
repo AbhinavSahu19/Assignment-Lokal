ce71d61893859c6a024333defea4161387c820bd
abhinav19 <sahu04abhi@gmail.com>
2025-05-06 00:00:03 +0530

    Initial commit - Hilt Application.

---
+++ app/src/main/java/com/example/assignmentlokal/AssignmentLokalApplication.kt
@@ -0,0 +1,8 @@
+package com.example.assignmentlokal
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AssignmentLokalApplication : Application(){
+}
