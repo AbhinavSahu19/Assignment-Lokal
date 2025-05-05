b32168289bd9fae0dd59a91027cc5d914c519159
abhinav19 <sahu04abhi@gmail.com>
2025-05-06 00:42:27 +0530

    Database

---
+++ app/src/main/java/com/example/assignmentlokal/local/JobDatabase.kt
@@ -0,0 +1,17 @@
+package com.example.assignmentlokal.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.assignmentlokal.local.dao.JobDao
+import com.example.assignmentlokal.local.dao.KeyDao
+import com.example.assignmentlokal.local.entity.JobEntity
+import com.example.assignmentlokal.local.entity.KeyEntity
+
+@Database(
+    entities = [JobEntity::class, KeyEntity::class],
+    version = 1
+)
+abstract class JobDatabase: RoomDatabase() {
+    abstract val jobDao: JobDao
+    abstract val keyDao: KeyDao
+}
