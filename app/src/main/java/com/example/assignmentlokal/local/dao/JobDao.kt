f325c91f55b29b118f0270ca254e4b41c1d5ad16
abhinav19 <sahu04abhi@gmail.com>
2025-05-06 14:23:07 +0530

    Added getById method.

--- app/src/main/java/com/example/assignmentlokal/local/dao/JobDao.kt
+++ app/src/main/java/com/example/assignmentlokal/local/dao/JobDao.kt
@@ -6,6 +6,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.assignmentlokal.local.entity.JobEntity
+import kotlinx.coroutines.flow.Flow
 
 
 @Dao
@@ -15,6 +16,9 @@ interface JobDao {
 
     @Query("SELECT * FROM job_table ORDER BY id desc")
     fun getAll(): PagingSource<Int, JobEntity>
+
+    @Query("SELECT * FROM job_table where id = :id")
+    fun getById(id: Int): Flow<JobEntity>
 
     @Query("DELETE FROM job_table")
     suspend fun clearAll()
