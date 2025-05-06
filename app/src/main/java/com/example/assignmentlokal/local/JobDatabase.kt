02eee61d9b6fc9f6a7a5ff384f1344e3d67afc46
abhinav19 <sahu04abhi@gmail.com>
2025-05-06 14:26:26 +0530

    Added Bookmark dao in db.

--- app/src/main/java/com/example/assignmentlokal/local/JobDatabase.kt
+++ app/src/main/java/com/example/assignmentlokal/local/JobDatabase.kt
@@ -2,16 +2,20 @@ package com.example.assignmentlokal.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.assignmentlokal.local.dao.BookmarkedJobsDao
 import com.example.assignmentlokal.local.dao.JobDao
 import com.example.assignmentlokal.local.dao.KeyDao
+import com.example.assignmentlokal.local.entity.BookmarkJobEntity
 import com.example.assignmentlokal.local.entity.JobEntity
 import com.example.assignmentlokal.local.entity.KeyEntity
 
 @Database(
-    entities = [JobEntity::class, KeyEntity::class],
+    entities = [JobEntity::class, KeyEntity::class, BookmarkJobEntity::class],
     version = 1
 )
 abstract class JobDatabase: RoomDatabase() {
     abstract val jobDao: JobDao
     abstract val keyDao: KeyDao
+    abstract val bookmarkDao: BookmarkedJobsDao
+
 }
